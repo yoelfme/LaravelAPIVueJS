013af9e7fc0c713e9773ccc5d6b8a50a517563cd
Yoel Monzon <yoelfme@hotmail.com>
2016-05-19 23:58:22 -0600

    [update] create directory to allow unit test to api

---
+++ tests/Api/ApiNoteTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Note;
+use App\Category;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ApiNoteTest extends TestCase
+{
+
+    use DatabaseTransactions;
+
+    public function test_list_notes()
+    {
+        $category = factory(Category::class)->create();
+
+        factory(Note::class)->times(2)->create([
+            'category_id' => $category->id
+        ]);
+
+        $this->get('api/v1/notes')
+            ->assertResponseOk()
+            ->seeJsonEquals(Note::all()->toArray());
+
+    }
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
